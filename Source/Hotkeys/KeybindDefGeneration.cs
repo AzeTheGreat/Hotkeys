09a1f3dadd22562a38d0acf12461a3b4af7a10b5
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2018-10-24 12:46:40 -0700

    Half tested direct hotkey def generation.
    Needs ui implementation for full testing.

--- Source/Hotkeys/KeybindDefGeneration.cs
+++ Source/Hotkeys/KeybindDefGeneration.cs
@@ -1,6 +1,8 @@
 ﻿using RimWorld;
 using Verse;
 using Harmony;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hotkeys
 {
@@ -10,12 +12,61 @@ namespace Hotkeys
     {
         static void Postfix()
         {
+            // Generate keybindings for all architect subtabs.
             foreach (var def in DefDatabase<DesignationCategoryDef>.AllDefsListForReading)
             {
                 var keyDef = new KeyBindingDef();
                 keyDef.category = DefDatabase<KeyBindingCategoryDef>.GetNamed("ArchitectHotkeys");
                 keyDef.defName = "ArchitectHotkeys_" + def.defName;
                 keyDef.label = def.label + " tab";
+                keyDef.defaultKeyCodeA = UnityEngine.KeyCode.None;
+                keyDef.modContentPack = def.modContentPack;
+                DefGenerator.AddImpliedDef<KeyBindingDef>(keyDef);
+
+                //var test = DefDatabase<DesignationCategoryDef>.GetNamed("balh").AllResolvedDesignators.Find(x => x.Label.Contains("blah"));
+            }
+
+            var settings = LoadedModManager.GetMod<Hotkeys>().GetSettings<HotkeySettings>();
+
+            // Sanitize settings - check for removed defs
+            var allDesCatDefs = DefDatabase<DesignationCategoryDef>.AllDefsListForReading.Select(defCat => defCat.label);
+
+            foreach (var desCat in settings.desCategories)
+            {
+                if (!allDesCatDefs.Contains(desCat))
+                {
+                    int index = settings.desCategories.IndexOf(desCat);
+                    settings.desCategories.RemoveAt(index);
+                    settings.designators.RemoveAt(index);
+                    settings.Write();
+                    return;
+                }
+
+                var allDesDefs = DefDatabase<DesignationCategoryDef>.GetNamed(desCat).AllResolvedDesignators.Select(des => des.Label);
+
+                foreach (var des in allDesDefs)
+                {
+                    if (!allDesDefs.Contains(des))
+                    {
+                        int index = settings.desCategories.IndexOf(desCat);
+                        settings.desCategories.RemoveAt(index);
+                        settings.designators.RemoveAt(index);
+                        settings.Write();
+                    }
+                }
+            }
+
+            // Generate keybindings for all direct hotkeys
+            foreach (var desCat in settings.desCategories)
+            {
+                int index = settings.desCategories.IndexOf(desCat);
+                string designator = settings.designators[index];
+                var def = DefDatabase<DesignationCategoryDef>.GetNamed(desCat);
+
+                var keyDef = new KeyBindingDef();
+                keyDef.category = DefDatabase<KeyBindingCategoryDef>.GetNamed("ArchitectHotkeys");
+                keyDef.defName = "ArchitectHotkeys_" + desCat + "_" + designator;
+                keyDef.label = desCat + ": " + designator;
                 keyDef.defaultKeyCodeA = UnityEngine.KeyCode.None;
                 keyDef.modContentPack = def.modContentPack;
                 DefGenerator.AddImpliedDef<KeyBindingDef>(keyDef);
--- Source/Hotkeys/ModUI.cs
+++ Source/Hotkeys/ModUI.cs
@@ -11,12 +11,45 @@ namespace Hotkeys
 {
     public class Hotkeys : Mod
     {
+        public override string SettingsCategory() => "Hotkeys";
+        private HotkeySettings settings;
+
         public Hotkeys(ModContentPack content) : base(content)
         {
+            // Initialize Harmony
             var harmonyHotkeys = HarmonyInstance.Create("Hotkeys");
             harmonyHotkeys.PatchAll(Assembly.GetExecutingAssembly());
+
+            settings = GetSettings<HotkeySettings>();
+        }
+
+        // The UI part
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            settings.desCategories[0] = Widgets.TextField(inRect.TopHalf().TopHalf(), settings.desCategories[0]);
+            settings.designators[0] = Widgets.TextField(inRect.BottomHalf().BottomHalf(), settings.designators[0]);
+
+            settings.Write();
+        }
+    }
+
+    public class HotkeySettings : ModSettings
+    {
+        public List<string> desCategories;
+        public List<string> designators;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref desCategories, "Designation_Categories");
+            Scribe_Collections.Look(ref designators, "Designators");
+
+            // If lists don't exist create them
+            if (desCategories == null) { desCategories = new List<string>();}
+            if (designators == null) { designators = new List<string>(); }
         }
     }
 }
+
 
 
