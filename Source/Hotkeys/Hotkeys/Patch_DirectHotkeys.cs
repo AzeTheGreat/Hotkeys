5ed20ca50a9e3bd99c2e1b0fd303025638eb8b16
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2018-12-11 09:37:41 -0700

    Moved Patch_CheckGizmoKeybindindings to separate file

--- Source/Hotkeys/Hotkeys/Patch_DirectHotkeys.cs
+++ Source/Hotkeys/Hotkeys/Patch_DirectHotkeys.cs
@@ -34,20 +34,6 @@ namespace Hotkeys
             DirectKeys.gizmoTriggered = false;
         }
     }
-
-    [HarmonyPatch(typeof(Command), nameof(Command.GizmoOnGUI))]
-    public class Patch_CheckGizmoKeybindings
-    {
-        static void Postfix(Command __instance)
-        {
-            if(__instance.hotKey == null) { return; }
-
-            if (__instance.hotKey.JustPressed)
-            {
-                DirectKeys.gizmoTriggered = true;
-            }
-        }
-    }
 }
 
 
---
+++ Source/Hotkeys/Utilities/Patch_CheckGizmoKeybindings.cs
@@ -0,0 +1,23 @@
+﻿using Harmony;
+using Verse;
+
+
+namespace Hotkeys
+{
+    [HarmonyPatch(typeof(Command), nameof(Command.GizmoOnGUI))]
+    public class Patch_CheckGizmoKeybindings
+    {
+        static void Postfix(Command __instance)
+        {
+            if(__instance.hotKey == null) { return; }
+
+            if (__instance.hotKey.JustPressed)
+            {
+                DirectKeys.gizmoTriggered = true;
+            }
+        }
+    }
+}
+
+
+
