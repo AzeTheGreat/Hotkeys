678858f87d4fb488dae08346800cba8016598f05
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2018-12-17 16:31:03 -0700

    Cache for KeyBindingDefs with fully specified keys

---
+++ Source/Hotkeys/Command Hotkeys/KeyBindFlagged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace Hotkeys
+{
+    class KeyBindFlagged
+    {
+        public KeyBindingDef keyDef;
+        public bool isUpdated;
+    }
+}
--- Source/Hotkeys/Command Hotkeys/Patch_ApplyGizmoHotkeys.cs
+++ Source/Hotkeys/Command Hotkeys/Patch_ApplyGizmoHotkeys.cs
@@ -9,26 +9,53 @@ namespace Hotkeys
     [HarmonyPatch(typeof(Command), nameof(Command.GizmoOnGUI))]
     class Patch_ApplyGizmoHotkeys
     {
+        static Dictionary<string, KeyBindFlagged> keyCache = new Dictionary<string, KeyBindFlagged>();
+
         static void Prefix(Command __instance)
         {
-            List<string> keys = __instance.KeyList();
+            string keyName = __instance.Key(true, true, true);
+            bool isCached = keyCache.TryGetValue(keyName, out KeyBindFlagged keyData);
+
+            if (!isCached || !keyData.isUpdated)
+            {
+                keyData = FullKeySearch(__instance);
+                keyCache[keyName] = keyData;
+            }
+
+            if (keyData.keyDef != null)
+            {
+                __instance.hotKey = keyData.keyDef;
+            }
+        }
+
+        private static KeyBindFlagged FullKeySearch(Command command)
+        {
+            Log.Message("Full Search");
+            List<string> keys = command.KeyList();
+            KeyBindFlagged keyCache = new KeyBindFlagged();
 
             foreach (string key in keys)
             {
                 DirectKeyData direct = DirectKeys.GetKey(key);
                 if (direct != null)
                 {
-                    __instance.hotKey = direct.keyDef;
-                    //return;
+                    keyCache.keyDef = direct.keyDef;
+                    keyCache.isUpdated = true;
+                    return keyCache;
                 }
 
                 GizmoKeyData gizmo = GizmoKeys.GetKey(key);
                 if (gizmo != null)
                 {
-                    __instance.hotKey = gizmo.keyDef;
-                    //return;
+                    keyCache.keyDef = gizmo.keyDef;
+                    keyCache.isUpdated = true;
+                    return keyCache;
                 }
             }
+
+            keyCache.keyDef = null;
+            keyCache.isUpdated = true;
+            return keyCache;
         }
     }
 
