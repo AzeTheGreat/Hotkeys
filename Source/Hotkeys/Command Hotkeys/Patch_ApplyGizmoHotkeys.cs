c64d665216a313067883bdb175f229a693807ec2
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2020-08-16 12:24:57 -0700

    When gizmo hotkeys are cleared the original hotkey will be restored.

--- Source/Hotkeys/Command Hotkeys/Patch_ApplyGizmoHotkeys.cs
+++ Source/Hotkeys/Command Hotkeys/Patch_ApplyGizmoHotkeys.cs
@@ -1,7 +1,6 @@
 ﻿using HarmonyLib;
-using Verse;
-using System.Collections;
 using System.Collections.Generic;
+using Verse;
 
 
 namespace Hotkeys
@@ -10,71 +9,61 @@ namespace Hotkeys
     class Patch_ApplyGizmoHotkeys
     {
         public static List<Command> newCommands = new List<Command>();
+
         static Dictionary<string, KeyBindFlagged> keyCache = new Dictionary<string, KeyBindFlagged>();
+        static Dictionary<string, KeyBindingDef> oldKeyCache = new Dictionary<string, KeyBindingDef>();
 
         static void Prefix()
         {
-            if(!Hotkeys.settings.useCommandHotkeys) { return; }
+            if (!Hotkeys.settings.useCommandHotkeys)
+                return;
 
             foreach (Command command in newCommands)
-            {
                 UpdateCommandHotkey(command);
-            }
+
             newCommands.Clear();
         }
 
         private static KeyBindFlagged FullKeySearch(Command command)
         {
-            List<string> keys = command.KeyList();
-            var keyCache = new KeyBindFlagged();
-
-            foreach (string key in keys)
+            foreach (string key in command.KeyList())
             {
                 DirectKeyData direct = DirectKeys.GetKey(key);
                 if (direct != null)
-                {
-                    keyCache.keyDef = direct.keyDef;
-                    keyCache.isUpdated = true;
-                    return keyCache;
-                }
+                    return new KeyBindFlagged() { keyDef = direct.keyDef };
 
                 GizmoKeyData gizmo = GizmoKeys.GetKey(key);
                 if (gizmo != null)
-                {
-                    keyCache.keyDef = gizmo.keyDef;
-                    keyCache.isUpdated = true;
-                    return keyCache;
-                }
+                    return new KeyBindFlagged() { keyDef = gizmo.keyDef };
             }
 
-            keyCache.keyDef = null;
-            keyCache.isUpdated = true;
-            return keyCache;
+            return new KeyBindFlagged();
         }
 
         private static void UpdateCommandHotkey(Command command)
         {
             string keyName = command.Key(true, true, true);
-            bool isCached = keyCache.TryGetValue(keyName, out KeyBindFlagged keyData);
 
-            if (!isCached || !keyData.isUpdated)
+            if (!oldKeyCache.TryGetValue(keyName, out KeyBindingDef oldKeyDef))
+                oldKeyCache[keyName] = oldKeyDef = command.hotKey;
+
+            if (!keyCache.TryGetValue(keyName, out KeyBindFlagged keyData) || !keyData.isUpdated)
             {
                 keyData = FullKeySearch(command);
                 keyCache[keyName] = keyData;
+                keyData.isUpdated = true;
             }
 
             if (keyData.keyDef != null)
-            {
                 command.hotKey = keyData.keyDef;
-            }
+            else
+                command.hotKey = oldKeyDef;
         }
 
         public static void UpdateCache()
         {
             foreach (KeyBindFlagged key in keyCache.Values)
-            {
                 key.isUpdated = false;
-            }
         }
     }
 }
