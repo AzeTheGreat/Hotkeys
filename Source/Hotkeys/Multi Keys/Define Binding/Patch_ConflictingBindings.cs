bdf6f33087bc91613b3f41fabc6c35a9e760346b
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2020-08-16 11:25:26 -0700

    Removed detour, fixing errors when keys have the same binding.

---
+++ Source/Hotkeys/Multi Keys/Define Binding/Patch_ConflictingBindings.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace Hotkeys
+{
+    [HarmonyPatch(typeof(KeyPrefsData), "ConflictingBindings")]
+    static class Patch_ConflictingBindings
+    {
+        static IEnumerable<KeyBindingDef> Postfix(IEnumerable<KeyBindingDef> conflictingDefs, KeyBindingDef keyDef, KeyCode code, KeyPrefsData __instance)
+        {
+            foreach (var def in conflictingDefs)
+            {
+                if (CheckAllKeys(keyDef, def, code, __instance))
+                    yield return def;
+            }
+        }
+
+        private static bool CheckAllKeys(KeyBindingDef assignedKeyDef, KeyBindingDef existingKeyDef, KeyCode assignedCode, KeyPrefsData __instance)
+        {
+            __instance.keyPrefs.TryGetValue(assignedKeyDef, out KeyBindingData prefDataAssigned);
+            __instance.keyPrefs.TryGetValue(existingKeyDef, out KeyBindingData prefDataExisting);
+
+            var assignedCodes = new List<KeyCode>();
+            var existingCodes = new List<KeyCode>();
+
+            if (assignedCode == prefDataAssigned.keyBindingA)
+                assignedCodes = assignedKeyDef.ModifierData().keyBindModsA;
+            if (assignedCode == prefDataAssigned.keyBindingB)
+                assignedCodes = assignedKeyDef.ModifierData().keyBindModsB;
+
+            if (prefDataExisting.keyBindingA == assignedCode)
+                existingCodes = existingKeyDef.ModifierData().keyBindModsA;
+            if (prefDataExisting.keyBindingB == assignedCode)
+                existingCodes = existingKeyDef.ModifierData().keyBindModsB;
+
+            return assignedCodes.OrderBy(i => i).SequenceEqual(existingCodes.OrderBy(i => i));
+        }
+    }
+}
---
+++ Source/Hotkeys/Multi Keys/Define Binding/Trans_EraseConflictingBindingsForKeyCode.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Verse;
+
+namespace Hotkeys
+{
+    // Clear modifiers as well when keybinds conflict.
+    [HarmonyPatch(typeof(KeyPrefsData), nameof(KeyPrefsData.EraseConflictingBindingsForKeyCode))]
+    public class Trans_EraseConflictingBindingsForKeyCode
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var targetA = AccessTools.Field(typeof(KeyBindingData), nameof(KeyBindingData.keyBindingA));
+            var targetB = AccessTools.Field(typeof(KeyBindingData), nameof(KeyBindingData.keyBindingB));
+
+            foreach (var i in instructions)
+            {
+                yield return i;
+
+                if (i.opcode == OpCodes.Stfld && i.OperandIs(targetA))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldloc_1);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Trans_EraseConflictingBindingsForKeyCode), nameof(Trans_EraseConflictingBindingsForKeyCode.ClearA)));
+                }
+                else if (i.opcode == OpCodes.Stfld && i.OperandIs(targetA))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldloc_1);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Trans_EraseConflictingBindingsForKeyCode), nameof(Trans_EraseConflictingBindingsForKeyCode.ClearA)));
+                } 
+            }
+        }
+
+        private static void ClearA(KeyBindingDef keyBindingDef) => Patch_KeyBindDrawing.ResetModifierList(KeyPrefs.BindingSlot.A, keyBindingDef);
+        private static void ClearB(KeyBindingDef keyBindingDef) => Patch_KeyBindDrawing.ResetModifierList(KeyPrefs.BindingSlot.A, keyBindingDef);
+    }
+}
